155b6f45e0323d7f4422d5034e400ab33b5f13cc
Miguel <miguelopez05@gmail.com>
2026-09-09 20:16:37 -0400

    implementação de diccionario de processos de soldagem e adicionamento de valores correspondentes

--- Sugestor de solda/main.py
+++ Sugestor de solda/main.py
@@ -2,17 +2,29 @@ import tkinter as tk
 from tkinter import ttk
 
 #Diccionario de material
-REGRAS_MATERIAIS ={
-    "Aço Carbono": (30, 40),
-    "Aço Inox": (25, 35),
-    "Aluminio": (40,50)
+REGRAS_MATERIAIS = {
+    "Aço Carbono": { 
+        "TIG":(30, 40),
+        "MIG":(40, 55)
+    },
+                
+    "Aço Inox": {
+        "TIG": (25, 35),
+        "MIG": (35, 45)
+    },
+
+    "Aluminio": {
+        "TIG": (40, 50),
+        "MIG": (50, 65)
+    }
 }
 
 def calcular_amperagem():
     try:
         espessura = float(entry_espessura.get())
         material = combo_material.get()
-        fator_min, fator_max = REGRAS_MATERIAIS[material]
+        processo = combo_processo.get()
+        fator_min, fator_max = REGRAS_MATERIAIS[material][processo]
         
         # Regra de negócio simpl.es: estimativa base de ~30 a 40 Amperes por milímetro de espessura.
         amperagem_min = espessura * fator_min
@@ -31,7 +43,7 @@ def calcular_amperagem():
 # Configuração da Janela Principal
 app = tk.Tk()
 app.title("Sugestor de Parâmetros de Soldagem")
-app.geometry("540x540")
+app.geometry("400x320")
 app.resizable(False, False)
 
 # Componentes da Interface
@@ -44,6 +56,12 @@ lbl_material.pack(pady=2)
 combo_material = ttk.Combobox(app, values=list(REGRAS_MATERIAIS.keys()), state="readonly")
 combo_material.set("Aço Carbono")
 combo_material.pack(pady=20)
+
+lbl_processo = ttk.Label(app, text="Selecione o processo de Soldagem:")
+lbl_processo.pack(pady=2)
+
+combo_processo = ttk.Combobox(app, values=list(REGRAS_MATERIAIS["Aço Carbono"]), state="readonly" )
+combo_processo.pack(pady=2)
 
 lbl_instrucao = ttk.Label(app, text="Espessura da Chapa (mm):")
 lbl_instrucao.pack(pady=5)
